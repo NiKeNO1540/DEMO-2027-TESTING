fb7028f023d46186bfe81c5196c7464f631c0de8
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2026-02-17 14:57:21 +0500

    Update Raid-HQ-SRV.sh

--- Raid-HQ-SRV.sh
+++ Raid-HQ-SRV.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-mdadm --create /dev/md0 --level=0 --raid-devices=2 /dev/sd[b-c]
-mdadm --detail -scan --verbose > /etc/mdadm.conf
-apt-get update && apt-get install fdisk -y
-fdisk /dev/md0 << EOF
+sudo mdadm --create /dev/md0 --level=0 --raid-devices=2 /dev/sd[b-c]
+sudo mdadm --detail -scan --verbose > /etc/mdadm.conf
+sudo apt-get update && sudo apt-get install fdisk -y
+sudo fdisk /dev/md0 << EOF
 n
 p
 1
@@ -12,23 +12,23 @@ p
 w
 EOF
 
-mkfs.ext4 /dev/md0p1
-cat << EOF >> /etc/fstab
+sudo mkfs.ext4 /dev/md0p1
+sudo cat << EOF >> /etc/fstab
 /dev/md0p1  /raid  ext4  defaults  0  0
 EOF
 
-mkdir /raid
-mount -a
+sudo mkdir /raid
+sudo mount -a
 
-apt-get install nfs-server -y
-mkdir /raid/nfs
-chown 99:99 /raid/nfs
-chmod 777 /raid/nfs
+sudo apt-get install nfs-server -y
+sudo mkdir /raid/nfs
+sudo chown 99:99 /raid/nfs
+sudo chmod 777 /raid/nfs
 
-cat << EOF >> /etc/exports
+sudo cat << EOF >> /etc/exports
 /raid/nfs  192.168.2.0/28(rw,sync,no_subtree_check)
 EOF
-exportfs -a
-exportfs -v
-systemctl enable nfs
-systemctl restart nfs
+sudo exportfs -a
+sudo exportfs -v
+sudo systemctl enable nfs
+sudo systemctl restart nfs
