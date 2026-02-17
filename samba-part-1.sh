f5a3180ee857afb1abad9378437c29e73772a888
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2026-02-17 14:58:54 +0500

    Update samba-part-1.sh

--- samba-part-1.sh
+++ samba-part-1.sh
@@ -1,24 +1,22 @@
 #! /bin/bash
 
-useradd sshuser -u 2026
-echo -e "sshuser:P@ssw0rd" | chpasswd
-timedatectl set-timezone Asia/Yekaterinburg
+sudo timedatectl set-timezone Asia/Yekaterinburg
 
 # Установка утилит
-echo nameserver 8.8.8.8 >> /etc/resolv.conf && apt-get update && apt-get install wget dos2unix task-samba-dc -y
+sudo echo nameserver 8.8.8.8 >> /etc/resolv.conf && sudo apt-get update && sudo apt-get install wget dos2unix task-samba-dc -y
 
 # Установка samba
-echo nameserver 192.168.1.10 >> /etc/resolv.conf
-echo 192.168.3.10 br-srv.au-team.irpo >> /etc/hosts
+sudo echo nameserver 192.168.1.10 >> /etc/resolv.conf
+sudo echo 192.168.3.10 br-srv.au-team.irpo >> /etc/hosts
 # Для HQ-SRV: echo server=/au-team.irpo/192.168.3.10 >> /etc/dnsmasq.conf
-rm -rf /etc/samba/smb.conf
-samba-tool domain provision --realm=AU-TEAM.IRPO --domain=AU-TEAM --adminpass=P@ssw0rd --dns-backend=SAMBA_INTERNAL --server-role=dc --option='dns forwarder=192.168.1.10'
-mv -f /var/lib/samba/private/krb5.conf /etc/krb5.conf
-systemctl enable --now samba.service
-samba-tool user add hquser1 P@ssw0rd
-samba-tool user add hquser2 P@ssw0rd
-samba-tool user add hquser3 P@ssw0rd
-samba-tool user add hquser4 P@ssw0rd
-samba-tool user add hquser5 P@ssw0rd
-samba-tool group add hq
-samba-tool group addmembers hq hquser1,hquser2,hquser3,hquser4,hquser5
+sudo rm -rf /etc/samba/smb.conf
+sudo samba-tool domain provision --realm=AU-TEAM.IRPO --domain=AU-TEAM --adminpass=P@ssw0rd --dns-backend=SAMBA_INTERNAL --server-role=dc --option='dns forwarder=192.168.1.10'
+sudo mv -f /var/lib/samba/private/krb5.conf /etc/krb5.conf
+sudo systemctl enable --now samba.service
+sudo samba-tool user add hquser1 P@ssw0rd
+sudo samba-tool user add hquser2 P@ssw0rd
+sudo samba-tool user add hquser3 P@ssw0rd
+sudo samba-tool user add hquser4 P@ssw0rd
+sudo samba-tool user add hquser5 P@ssw0rd
+sudo samba-tool group add hq
+sudo samba-tool group addmembers hq hquser1,hquser2,hquser3,hquser4,hquser5
