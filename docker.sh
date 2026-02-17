a625355949615cf5a4c6314d59e7d94b29f8972d
NiKeNO1540 <66269872+NiKeNO1540@users.noreply.github.com>
2026-02-17 14:54:38 +0500

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-apt-get update && apt-get install -y docker-compose docker-engine
-systemctl enable --now docker
-mount -o loop /dev/sr0
-docker load -i /media/ALTLinux/docker/site_latest.tar
-docker load -i /media/ALTLinux/docker/mariadb_latest.tar
+sudo apt-get update && sudo apt-get install -y docker-compose docker-engine
+sudo systemctl enable --now docker
+sudo mount -o loop /dev/sr0
+sudo docker load -i /media/ALTLinux/docker/site_latest.tar
+sudo docker load -i /media/ALTLinux/docker/mariadb_latest.tar
 
 
-cat << EOF >> launch.sh
+sudo cat << EOF >> launch.sh
 docker compose -f site.yml up -d 
 sleep 5 
 docker exec -it db mysql -u root -pPassw0rd -e "
